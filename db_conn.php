3f25bff335b145b4740d31099b18b215a85c95e9
thanhstar <74256242+thanhstar@users.noreply.github.com>
2021-06-29 21:51:46 +0700

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
 <?php
 
-$sname = "localhost";
-$uname = "root";
-$password = "";
+$sname = "52.6.114.59";
+$uname = "chithanh";
+$password = "123@123a";
 
 $db_name = "shopmusic";
 $conn = mysqli_connect($sname,$uname, $password,$db_name);
