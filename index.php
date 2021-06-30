45ccd3028333bbc45ccdf593ca939cd79c1f322c
thanhstar <74256242+thanhstar@users.noreply.github.com>
2021-06-30 12:51:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,7 @@ session_start();
 include('inc/header.php'); 
 include('inc/navbar.php');}
 
-?>
-<?php
-// include('inc/side.php');
 include('inc/listsong.php');include('inc/scripts.php');
-?>
 
-<?php
 include('inc/footer.php');
 ?>
